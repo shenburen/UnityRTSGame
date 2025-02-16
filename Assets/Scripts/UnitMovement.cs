93de1a68c160f3d2704d3679df86698668526c54
shenzhiqiang <593446878@qq.com>
2025-02-16 22:57:56 +0800

    单位移动。

--- Assets/Scripts/UnitMovement.cs
+++ Assets/Scripts/UnitMovement.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class UnitMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Camera cam;
+    private NavMeshAgent agent;
+    public LayerMask ground;
+
+    private void Start()
     {
-        
+        cam = Camera.main;
+        agent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, ground))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
     }
 }
